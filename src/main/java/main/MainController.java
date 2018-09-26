f044d598277f59ec347ba56318a72d51ba33048c
tomkow95 <tom.kow@poczta.pl>
2018-09-26 21:16:47 +0200

    sadfasdf

---
+++ src/main/java/main/MainController.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainController {
+
+    public static void main(String[] args) {
+
+    }
+}
